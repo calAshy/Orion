61dde06e79af90ca1d5978d3f11661f3b929ea9d
calAshy <22518156@stu.mmu.ac.uk>
2025-09-25 16:37:11 +0100

    Positioned button and started footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,34 @@
 import Logo from "../Images/LogoWhite.png";
+import './Footer.css';
 
-function Footer(){
+function Footer() {
   return (
-    <div className="FooterContaier">
+    <div className="FooterContainer">
       <div className="LogoSocialsContainer">
            <img className="FooterLogo" src={Logo} alt="Orion company logo "/>
       </div>
       <div className="FooterNavContainer">
-        <div className="NavClmOne"></div>
-        <div className="NavClmTwo"></div>
-        <div className="NavClmThree"></div>
+        <div className="NavColumn">
+          <ul className="NavColumnUL">
+            <li className="NavColumnLI">Link</li>
+            <li className="NavColumnLI">Link</li>
+            <li className="NavColumnLI">Link</li>
+          </ul>
+        </div>
+        <div className="NavColumn">
+          <ul className="NavColumnUL">
+            <li className="NavColumnLI">Link</li>
+            <li className="NavColumnLI">Link</li>
+            <li className="NavColumnLI">Link</li>
+          </ul>
+        </div>
+        <div className="NavColumn">
+          <ul className="NavColumnUL">
+            <li className="NavColumnLI">Link</li>
+            <li className="NavColumnLI">Link</li>
+            <li className="NavColumnLI">Link</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
